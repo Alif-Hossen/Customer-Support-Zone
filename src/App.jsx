9e7139f7350c62725f128b7de41d0bfe0e218543
Alif-Hossen <naimuralif1.0@gmail.com>
2025-09-26 07:44:22 -0600

    Fotter Section Responsive Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Footer from './components/Footer'
 import Navbar from './components/navbar'
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Footer></Footer>
     </>
   )
 }
