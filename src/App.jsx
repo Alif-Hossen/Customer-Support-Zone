82a4d674e89bae56c89a790af3ce6a3960f1880f
Alif-Hossen <naimuralif1.0@gmail.com>
2025-09-26 05:23:25 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/navbar'
+
+function App() {
+  return (
+    <>
+      <p>kire</p>
+      <Navbar></Navbar>
+      <h3>HOY NA KNO?</h3>
+    </>
+  )
+}
+
+export default App
