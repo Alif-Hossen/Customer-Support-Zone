314abce1a618e750cc586976b9dcf87a57f1abff
Alif-Hossen <naimuralif1.0@gmail.com>
2025-09-27 08:50:33 -0600

    Deployment: Final data path fix and deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,54 +1,45 @@
-// MAIN -->
-
 import React from 'react';
-import Twiter from './Images/Vector.png'
+import Twiter from './Images/Vector.png';
 
 const Footer = () => {
-    return (
-        <>
-            <div className='mb-10'>
-                <div className='bg-black p-20 text-white mt-30 grid grid-cols-1 md:grid-cols-5 pl-40  border-b-1 '>
-
-                    <div className=' '>
-                        <h4 className='text-2xl font-bold'>CS — Ticket System</h4>
-                        <p className='pt-6 text-gray-400'>To Be Honest - Last Time I Was Not Attentive In My  Course. And i Don't Even Complete My Last 2 Module  Even The Project. Though I Had Completed Till Video-4  After All Now I Am here To Do Assignment Seven. I Just  Want to Complete My Assignment WithOut Using AI Power Tolls. </p>
-                    </div>
-                    <div className='pt-8 md:pt-0 md:pl-20'>
-                        <h4 className='text-2xl font-bold'>Company</h4>
-                        <p className='pt-2 md:pt-6 text-gray-400'>About Us</p>
-                        <p className='pt-2 md:pt-6 text-gray-400'>Our Mission</p>
-                        <p className='pt-2 md:pt-6 text-gray-400'>Contact Sales</p>
-                    </div>
-                    <div className='pt-8 md:pt-0 '>
-                        <h4 className='text-2xl font-bold'>Services</h4>
-                        <p className='pt-2 md:pt-6 text-gray-400'>Products & Services</p>
-                        <p className='pt-2 md:pt-6 text-gray-400'>ProCustomer Stories</p>
-                        <p className='pt-2 md:pt-6 text-gray-400'>Download Apps</p>
-                    </div>
-                    <div className='pt-8  md:pt-0 '>
-                        <h4 className='text-2xl font-bold'>Information</h4>
-                        <p className='pt-2 md:pt-6 text-gray-400'>Privacy Policy</p>
-                        <p className='pt-2 md:pt-6 text-gray-400'>terms & Conditions Policy</p>
-                        <p className='pt-2 md:pt-6 text-gray-400'>Join Us</p>
-                    </div>
-                    <div className='pt-8 md:pt-0 '>
-                        <h4 className='text-2xl font-bold'>Social Links</h4>
-                        <p className='pt-2 md:pt-4 text-gray-400'>@CS — Ticket System</p>
-                        <p className='pt-2 md:pt-6 text-gray-400'>@CS — Ticket System</p>
-                        <p className='pt-2 md:pt-6 text-gray-400'>@CS — Ticket System</p>
-                        <p className='pt-2 md:pt-6 text-gray-400'>support@cst.com</p>
-                        
-                    </div>
-
-                </div>
-
-                <div className='bg-black text-center pt-6 pb-6' >
-                    <small>© 2025 CS — Ticket System. All rights reserved.</small>
-                </div>
-            </div>
-            
-        </>
-    );
+  return (
+    <>
+      <div className='bg-black p-20 text-white mt-30 grid grid-cols-1 md:grid-cols-5 pl-40 border-b-1'>
+        <div className=''>
+          <h4 className='text-2xl font-bold'>CS — Ticket System</h4>
+          <p className='pt-6 text-gray-400'>To Be Honest - Last Time I Was Not Attentive In My Course. And i Don't Even Complete My Last 2 Module Even The Project. Though I Had Completed Till Video-4 After All Now I Am here To Do Assignment Seven. I Just Want to Complete My Assignment WithOut Using AI Power Tolls. Here Point To Be Noted That I Don't use Any Kind Of AI Tools. I Had Submitted Last Six Assignment With My Own.. Like Previous I Am Here Again To Complete My Assignment.</p>
+        </div>
+        <div className='pt-8 md:pt-0 md:pl-20'>
+          <h4 className='text-2xl font-bold'>Company</h4>
+          <p className='pt-2 md:pt-6 text-gray-400'>About Us</p>
+          <p className='pt-2 md:pt-6 text-gray-400'>Our Mission</p>
+          <p className='pt-2 md:pt-6 text-gray-400'>Contact Sales</p>
+        </div>
+        <div className='pt-8 md:pt-0'>
+          <h4 className='text-2xl font-bold'>Services</h4>
+          <p className='pt-2 md:pt-6 text-gray-400'>Products & Services</p>
+          <p className='pt-2 md:pt-6 text-gray-400'>ProCustomer Stories</p>
+          <p className='pt-2 md:pt-6 text-gray-400'>Download Apps</p>
+        </div>
+        <div className='pt-8 md:pt-0'>
+          <h4 className='text-2xl font-bold'>Information</h4>
+          <p className='pt-2 md:pt-6 text-gray-400'>Privacy Policy</p>
+          <p className='pt-2 md:pt-6 text-gray-400'>terms & Conditions Policy</p>
+          <p className='pt-2 md:pt-6 text-gray-400'>Join Us</p>
+        </div>
+        <div className='pt-8 md:pt-0'>
+          <h4 className='text-2xl font-bold'>Social Links</h4>
+          <p className='pt-2 md:pt-4 text-gray-400'>@CS — Ticket System</p>
+          <p className='pt-2 md:pt-6 text-gray-400'>@CS — Ticket System</p>
+          <p className='pt-2 md:pt-6 text-gray-400'>@CS — Ticket System</p>
+          <p className='pt-2 md:pt-6 text-gray-400'>support@cst.com</p>
+        </div>
+      </div>
+      <div className='bg-black text-center pt-6 pb-6'>
+        <small>© 2025 CS — Ticket System. All rights reserved.</small>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,19 @@
-// MAIN -->
-
-
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <div class=" md:flex  justify-between max-w-[1310px] mx-auto pt-10">
-      
-      <div class=" ">
-        <h4 className='text-3xl font-bold text-center text-black'>CS--Ticket System</h4>
+    <div className="md:flex justify-between max-w-[1320px] mx-auto mt-10">
+      <div className="">
+        <h4 className='text-3xl font-bold text-center'>CS--Ticket System</h4>
       </div>
-
-      <div class="md:flex justify-between items-center flex flex-col md:flex-row md:gap-10 gap-4 md:pt-0 pt-8">
-        <a class="text-black " href=''>Home</a>
-        <a class=" text-black " href=''>FAQ</a>
-        <a class=" text-black " href="">Changelog</a>
-        <a class=" text-black " href="">Blog</a>
-        <a class=" text-black " href="">Download</a>
-        <a class=" text-black " href="">Contact</a>
-        <button className='bg-gradient-to-r from-[#6731E4] to-[#9C5FF1] text-white font-bold px-4 py-2 rounded-l rounded mr-2'> + New Ticket </button>
+      <div className="md:flex justify-between items-center flex flex-col md:flex-row md:gap-10 gap-4 md:pt-0 pt-8">
+        <a className=" " href=''>Home</a>
+        <a className=" " href=''>FAQ</a>
+        <a className=" " href="">Changelog</a>
+        <a className=" " href="">Blog</a>
+        <a className=" " href="">Download</a>
+        <a className=" " href="">Contact</a>
+        <button className='bg-violet-500 text-white font-bold p-3 rounded-xl'>+ New Ticket</button>
       </div>
     </div>
   );
